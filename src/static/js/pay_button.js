658f4f50187be6851e998dd61cff18602f94815f
federiconitidi <federiconitidi@hotmail.it>
2019-08-13 07:46:31 +0200

    normal

--- src/static/js/pay_button.js
+++ src/static/js/pay_button.js
@@ -200,7 +200,7 @@ function payForProduct(contract_address, product_id, priceInWei, customer_id, ac
             full_data = JSON.parse(localStorage['pending_transactions'])
             full_data = $.merge(full_data, [{'type' : 'payForProduct', 'hash' : result, 'status' : 'pending', 'metadata' : sessionStorage['contract_address'], 'account' : sessionStorage['account_address']}]);
             localStorage['pending_transactions'] = JSON.stringify(full_data)
-            document.getElementById("pay_button_form").innerHTML = '<span style="color:green; font-size:30pt">&#10004;</span>'
+            document.getElementById("pay_button_form").innerHTML = '<span style="color:green; font-size:30pt">&#10004; </span><span style="color:green; font-size:16pt">Purchase completed</span>'
             
         } else {
             console.log('error');
